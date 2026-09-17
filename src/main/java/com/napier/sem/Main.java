17d92339b0baaaf94550060d49f0cbe9cb20627c
Yuen Lin <40865089@live.napier.ac.uk>
2026-09-16 20:31:27 -0600

    connected Main.java to MongoDB container

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -1,22 +1,32 @@
 package com.napier.sem;
 
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
+import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 
 public class Main {
     public static void main(String[] args) {
-        // Connect to the local MongoDB Docker container instance
-        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
-            System.out.println("Connecting to database...");
+        // Connect to MongoDB on local system using port 27000
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
 
-            // Get database (creates it if it doesn't exist)
-            MongoDatabase database = mongoClient.getDatabase("test_db");
+        // Get database - creates it if it doesn't exist
+        MongoDatabase database = mongoClient.getDatabase("mydb");
 
-            System.out.println("Successfully linked to MongoDB container!");
-        } catch (Exception e) {
-            System.out.println("Failed to connect to MongoDB container.");
-            e.printStackTrace();
-        }
+        // Get collection
+        MongoCollection<Document> collection = database.getCollection("test");
+
+        // Create document
+        Document doc = new Document("name", "Your Name")
+                .append("class", "DevOps")
+                .append("year", "2024")
+                .append("result", new Document("CW", 95).append("EX", 85));
+
+        // Insert document into collection
+        collection.insertOne(doc);
+
+        // Retrieve and print document
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
     }
 }
