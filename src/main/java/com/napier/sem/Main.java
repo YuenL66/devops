849eb10bc24f78f1edc47ad0235524c76ca315d5
Yuen Lin <40865089@live.napier.ac.uk>
2026-09-21 14:08:17 -0600

    Implement multi-table SQL join for view employee record feature

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -103,9 +103,21 @@ public class Main
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
+                    "SELECT e.emp_no, e.first_name, e.last_name, "
+                            + "t.title, s.salary, d.dept_name, "
+                            + "m.first_name AS mgr_first, m.last_name AS mgr_last "
+                            + "FROM employees e "
+                            + "JOIN titles t ON e.emp_no = t.emp_no "
+                            + "JOIN salaries s ON e.emp_no = s.emp_no "
+                            + "JOIN dept_emp de ON e.emp_no = de.emp_no "
+                            + "JOIN departments d ON de.dept_no = d.dept_no "
+                            + "JOIN dept_manager dm ON de.dept_no = dm.dept_no "
+                            + "JOIN employees m ON dm.emp_no = m.emp_no "
+                            + "WHERE e.emp_no = " + ID
+                            + " AND t.to_date = '9999-01-01'"
+                            + " AND s.to_date = '9999-01-01'"
+                            + " AND de.to_date = '9999-01-01'"
+                            + " AND dm.to_date = '9999-01-01'";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
@@ -116,6 +128,10 @@ public class Main
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
+                emp.title = rset.getString("title");
+                emp.salary = rset.getInt("salary");
+                emp.dept_name = rset.getString("dept_name");
+                emp.manager = rset.getString("mgr_first") + " " + rset.getString("mgr_last");
                 return emp;
             }
             else
